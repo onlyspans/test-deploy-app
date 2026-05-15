0670b467c5137b93322a7b82dd6e11c07f69a61d
milkylake <126706140+milkylake@users.noreply.github.com>
2026-05-15 11:46:10 +0300

    test

--- app.js
+++ app.js
@@ -186,7 +186,7 @@ const server = http.createServer((req, res) => {
   }
   if (path === '/') {
     const html = pageShell({
-      title: 'Deploy app is up 123123',
+      title: 'Deploy app is up Привет!',
       subtitle: 'If you see this page, routing and the container runtime look good.',
       badge: 'ok',
       pageMessage,
